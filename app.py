25b10d9231901de823169e8ebad5ac025ae2a768
Seyi Pythonian <prontomaster@gmail.com>
2022-10-29 08:18:44 +0100

    removed the app factory pattern

--- app.py
+++ app.py
@@ -1,22 +1,17 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 
-def create_app():
-    """
-    Creates a Flask application using the app factory pattern.
-    :return: Flask app
-    """
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
-    app = Flask(__name__)
-    CORS(app, resources={r"/*": {"origins": "*"}})
+@app.route('/')
+def index():
+    """ Render a JSON response. """
+    return jsonify(
+        slackUsername="Pythonian",
+        backend=True,
+        age=27,
+        bio="An introvert that likes to make things happen on the web.")
 
-    @app.route('/')
-    def index():
-        """ Render a JSON response. """
-        return jsonify(
-            slackUsername="Pythonian",
-            backend=True,
-            age=27,
-            bio="An introvert that likes to make things happen on the web.")
-
-    return app
+if __name__ == '__main__':
+    app.run()
