12d878dd1a5316963a1f62bbb48128ffdb4821cc
Seyi Pythonian <prontomaster@gmail.com>
2022-11-05 11:41:08 +0100

    feat: stage2 task

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -12,6 +12,31 @@ def index():
         backend=True,
         age=27,
         bio="An introvert that likes to make things happen on the web.")
+
+
+@app.route('/calculate/', methods=['POST'])
+def calculate():
+    x = request.json.get('x', None)
+    y = request.json.get('y', None)
+    operation_type = request.json.get('operation_type', None)
+
+    if operation_type is not None and x is not None and y is not None:
+        if operation_type == "addition":
+            result = x + y
+        elif operation_type == "subtraction":
+            result = x - y
+        elif operation_type == "multiplication":
+            result = x * y
+        else:
+            result = None
+    else:
+        return None
+
+    return jsonify(
+        slackUsername="Pythonian",
+        operation_type=operation_type,
+        result=result)
+
 
 if __name__ == '__main__':
     app.run()
