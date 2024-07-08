d3eeb1b0fd44320aa54e890690b32ca0b9f777ae
Seyi Pythonian <prontomaster@gmail.com>
2024-07-08 09:22:03 +0100

    remove trailing slash in url

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,42 +1,48 @@
 from django.urls import path
 
-from .views import (add_user_to_organisation, create_organisation,
-                    get_organisation, get_organisations, get_user, login,
-                    register)
+from .views import (
+    add_user_to_organisation,
+    create_organisation,
+    get_organisation,
+    get_organisations,
+    get_user,
+    login,
+    register,
+)
 
 urlpatterns = [
     path(
-        "auth/register/",
+        "auth/register",
         register,
         name="register",
     ),
     path(
-        "auth/login/",
+        "auth/login",
         login,
         name="login",
     ),
     path(
-        "api/users/<str:id>/",
+        "api/users/<str:id>",
         get_user,
         name="get_user",
     ),
     path(
-        "api/organisations/",
+        "api/organisations",
         get_organisations,
         name="get_organisations",
     ),
     path(
-        "api/organisations/<str:orgId>/",
+        "api/organisations/<str:orgId>",
         get_organisation,
         name="get_organisation",
     ),
     path(
-        "api/organisations/",
+        "api/organisations",
         create_organisation,
         name="create_organisation",
     ),
     path(
-        "api/organisations/<str:orgId>/users/",
+        "api/organisations/<str:orgId>/users",
         add_user_to_organisation,
         name="add_user_to_organisation",
     ),
